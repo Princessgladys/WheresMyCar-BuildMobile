670dfc90fbeb33ddb898be3ea2eab8efba6ca329
David Meredith <david@meredithdev.com>
2013-02-22 15:11:08 -0800

    Part 4

---
+++ js/functions.js
@@ -0,0 +1,30 @@
+﻿/// <reference path="jquery-1.7-vsdoc.js" />
+function checkRequirements() {
+    if (navigator.network.connection.type == Connection.NONE) {
+        navigator.notification.alert(
+            'To use this app you must enable your internet connection',
+            function () { },
+            'Warning'
+            );
+        return false;
+            
+    }
+    return true;
+}
+
+function updateIcons()
+{
+    if ($(window).width() > 480)
+    {
+        $('a[data-icon], button[data-icon]').each(
+            function () {
+                $(this).removeAttr('data-iconpos');
+            });
+    }
+    else {
+        $('a[data-icon], button[data-icon]').each(
+            function () {
+                $(this).attr('data-iconpos', 'notext');
+            });
+    }
+}
---
+++ js/jquery.mobile.config.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="jquery.mobile-1.2.0.js" />
+
+$(document).bind('mobileinit',
+    function () {
+        //Default pages' transition effect
+        $.mobile.defaultPageTransition = "slide";
+       
+        // Page loader Widget
+        $.mobile.loader.prototype.options.textVisible = true;
+
+        // theme
+        $.mobile.page.prototype.options.theme = 'b';
+        $.mobile.page.prototype.options.headerTheme = 'b';
+        $.mobile.page.prototype.options.contentTheme = 'b';
+        $.mobile.page.prototype.options.footerTheme = 'b';
+        $.mobile.page.prototype.options.backBtnTheme = 'b';
+});
